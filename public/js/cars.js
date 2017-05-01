d9aaf8cb93919b3eca9f7cf424f651d0df7bd285
Rob Weber <rob.weber87@gmail.com>
2017-05-01 18:26:21 -0400

    Done.

--- public/js/cars.js
+++ public/js/cars.js
@@ -2,20 +2,39 @@
 
 // this is the base API url
 var baseUrl = "http://mimeocarlisting.azurewebsites.net/api/cars/";
+var pagenation = 3;
 
 function formatCars(carsJSON) {
+
   // this function shold return a string of properly formatted html
   // refer to app/views/index.erb lines 16 - 22 for an example of how
   // to format three cars, each in a div with a class "col-md-4", in a 
   // div with a class "row"
+  return carsJSON.map(function(car) {
+  		var car = 	`<div class="col-md-4 car"><h2>${car.Make}</h2><p><strong>Model:</strong> ${car.Model}</p><p><strong>Year:</strong> ${car.Year}</p></div>`
+	    return car
+  })
+  	return `<div class="row">
+				${car.join("")}
+  			</div>`
+
 }
 
 function addCarsToDOM(carsJSON) {
   // this function should pass carsJSON to formatCars() and then 
   // add the resulting HTML to the div with an id of "cars"
+  	$('#cars').append(formatCars(carsJSON));
 }
 
 function fetchJSON() {
+
+	$.ajax({
+		method: "GET",
+		url: `http://mimeocarlisting.azurewebsites.net/api/cars/${pagenation}/3`,
+		dataType: 'jsonp',
+		success: addCarsToDOM
+	})
+	pagenation ++;
   // this function will make the ajax call
   // on success of the ajax call, it will pass the returned data
   // to addCarsToDOM()
--- public/js/on-click.js
+++ public/js/on-click.js
@@ -3,4 +3,8 @@
 $(document).ready(function() {
   // add click listener here
   // it should call on fetchJSON()
+
+  $('#load-cars').on('click', function() {
+  	fetchJSON();
+  });
 });
